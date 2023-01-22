6601a6e9204c7681b92ccafe60a07fb6f16ddbd1
Maryna-Korbet <m22v86k@gmail.com>
2023-01-22 08:32:39 +0200

    added changeFilter, useSearchParams

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -2,17 +2,23 @@ import { getCustomers } from '../fakeAPI';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { SearchBox } from 'components/SearchBox';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 export const Customers = () => {
   const [customers, setCustomers] = useState([]);
+  const [searchParams, setsearchParams] = useSearchParams();
 
   useEffect(() => {
     getCustomers().then(setCustomers);
   }, []);
 
+  const changeFilter = value => {
+    setsearchParam({filter: value})
+  };
+
   return (
     <main>
-    <SearchBox />
+    <SearchBox onChange={changeFilter} />
       {customers.length > 0 && (
         <ul>
           {customers.map(customer => (
