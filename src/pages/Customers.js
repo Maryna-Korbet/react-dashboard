7b04f78e06f2f49b17495cbc8fd810c5eaf46525
Maryna-Korbet <m22v86k@gmail.com>
2023-01-22 08:42:50 +0200

    added filter

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -2,26 +2,29 @@ import { getCustomers } from '../fakeAPI';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { SearchBox } from 'components/SearchBox';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 export const Customers = () => {
   const [customers, setCustomers] = useState([]);
-  const [searchParams, setsearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filter = searchParams.get('filter') ?? "";
 
   useEffect(() => {
     getCustomers().then(setCustomers);
   }, []);
 
   const changeFilter = value => {
-    setsearchParam({filter: value})
+    setSearchParams(value !=="" ? {filter: value} : {});
   };
+
+  const visibleCustomers = customers.filter(customer => customer.name.toLowerCase().includes(filter.toLowerCase()));
 
   return (
     <main>
     <SearchBox onChange={changeFilter} />
-      {customers.length > 0 && (
+      {visibleCustomers.length > 0 && (
         <ul>
-          {customers.map(customer => (
+          {visibleCustomers.map(customer => (
             <li key={customer.id}>
               {customer.name}
             </li>
